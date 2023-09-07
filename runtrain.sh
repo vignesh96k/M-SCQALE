02bd1f0245af29179f20e3e54d6165a500275d2c
Kannan <vikanna@apac.corpdir.net>
2023-09-07 14:56:09 +0530

    bash file

---
+++ runtrain.sh
@@ -0,0 +1,5 @@
+python TRAIN.py --level 0 --bs 4 --k 10
+python TRAIN.py --level 1 --bs 4 --k 10
+python TRAIN.py --level 2 --bs 8 --k 20
+python TRAIN.py --level 3 --bs 16 --k 40
+python TRAIN.py --level 3 --bs 32 --k 40 --lowpass
